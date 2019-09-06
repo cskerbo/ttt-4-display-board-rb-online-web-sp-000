1052d46148b2f91b8368a93e6aae0794e18ee555
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-06 17:53:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]}  "
 end
 
-board = [" "," ","X"," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 display_board(board)
